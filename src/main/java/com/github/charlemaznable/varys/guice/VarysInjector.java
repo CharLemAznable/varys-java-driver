66a55db6babd10eccb095a0d8320b3df966c51bd
CharLemAznable <545541819@qq.com>
2020-02-26 03:20:39 +0800

    Simplify VarysInjector.

--- src/main/java/com/github/charlemaznable/varys/guice/VarysInjector.java
+++ src/main/java/com/github/charlemaznable/varys/guice/VarysInjector.java
@@ -5,59 +5,58 @@ import com.github.charlemaznable.core.net.ohclient.OhInjector;
 import com.github.charlemaznable.varys.config.VarysConfig;
 import com.github.charlemaznable.varys.impl.Query;
 import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.util.Modules;
 import com.google.inject.util.Providers;
 
 import static com.github.charlemaznable.core.lang.Listt.newArrayList;
 import static com.google.common.collect.Iterables.concat;
 
-public class VarysInjector extends OhInjector {
+public final class VarysInjector {
 
-    public VarysInjector(Module... modules) {
-        super(modules);
+    private OhInjector ohInjector;
+
+    public VarysInjector() {
+        this(Modules.EMPTY_MODULE);
     }
 
-    public VarysInjector(Iterable<? extends Module> modules) {
-        super(modules);
+    public VarysInjector(Class<? extends VarysConfig> configClass) {
+        this(new MinerInjector().createModule(configClass));
     }
 
-    public VarysInjector(Class<? extends VarysConfig> configClass,
-                         Module... otherModules) {
-        this(configClass, newArrayList(otherModules));
-    }
-
-    public VarysInjector(Class<? extends VarysConfig> configClass,
-                         Iterable<? extends Module> otherModules) {
-        this(new MinerInjector().createModule(configClass), otherModules);
-    }
-
-    public VarysInjector(VarysConfig configImpl,
-                         Module... otherModules) {
-        this(configImpl, newArrayList(otherModules));
-    }
-
-    public VarysInjector(VarysConfig configImpl,
-                         Iterable<? extends Module> otherModules) {
+    public VarysInjector(VarysConfig configImpl) {
         this(new AbstractModule() {
             @Override
             protected void configure() {
                 bind(VarysConfig.class).toProvider(Providers.of(configImpl));
             }
-        }, otherModules);
+        });
     }
 
-    public VarysInjector(Module configModule,
-                         Iterable<? extends Module> otherModules) {
-        this(concat(newArrayList(configModule), otherModules));
+    public VarysInjector(Module configModule) {
+        this.ohInjector = new OhInjector(configModule);
     }
 
-    @Override
     public Module createModule(Class... otherClientClasses) {
-        return this.createModule(newArrayList(otherClientClasses));
+        return createModule(newArrayList(otherClientClasses));
     }
 
-    @Override
     public Module createModule(Iterable<Class> otherClientClasses) {
-        return super.createModule(concat(newArrayList(Query.class), otherClientClasses));
+        return this.ohInjector.createModule(concat(
+                newArrayList(Query.class), otherClientClasses));
+    }
+
+    public Injector createInjector(Class... otherClientClasses) {
+        return createInjector(newArrayList(otherClientClasses));
+    }
+
+    public Injector createInjector(Iterable<Class> otherClientClasses) {
+        return Guice.createInjector(createModule(otherClientClasses));
+    }
+
+    public <T> T getClient(Class<T> ohClass) {
+        return this.ohInjector.getClient(ohClass);
     }
 }
