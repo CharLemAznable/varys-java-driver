9dbc93a3122b232ad10dd0163de77e900d543b15
CharLemAznable <545541819@qq.com>
2023-02-23 15:53:19 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/varys/spring/VarysConfigurer.java
+++ src/main/java/com/github/charlemaznable/varys/spring/VarysConfigurer.java
@@ -1,21 +1,23 @@
 package com.github.charlemaznable.varys.spring;
 
 import com.github.charlemaznable.core.spring.ElvesImport;
-import com.github.charlemaznable.httpclient.ohclient.OhScannerRegistrar;
+import com.github.charlemaznable.httpclient.ohclient.OhScan;
 import com.github.charlemaznable.varys.config.VarysConfig;
 import com.github.charlemaznable.varys.impl.Query;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.lang.Nullable;
 
 @Configuration
 @ElvesImport
+@OhScan(basePackageClasses = Query.class,
+        includeFilters = {@ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {Query.class}
+        )})
 public class VarysConfigurer {
-
-    @Bean("com.github.charlemaznable.varys.impl.Query")
-    public OhScannerRegistrar.OhClientFactoryBean query() {
-        return OhScannerRegistrar.buildFactoryBean(Query.class);
-    }
 
     @Bean("com.github.charlemaznable.varys.config.VarysConfig.ProxyWechatAppConfig")
     public VarysConfig.ProxyWechatAppConfig proxyWechatAppConfig(@Nullable VarysConfig varysConfig) {
